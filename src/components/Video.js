db5063376cc8b06bd4193e5673fe911924871e75
Mahfuj Ahmed <ahmed.mahfuj02@gmail.com>
2022-09-24 21:38:40 +0600

    Videos functionality added.

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,12 @@
 import classes from '../styles/Video.module.css';
-import cover from '../assets/images/3.jpg'
-export default function Video(){
+export default function Video({title, id, noq}){
     return(
             <div className={classes.video}>
-              <img src={cover} alt="Module" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+              <img src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`} alt={title} />
+              <p>{title}</p>
               <div className={classes.qmeta}>
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
+                <p>Question {noq}</p>
+                <p>Total point: {noq * 5}</p>
               </div>
             </div>
     )
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,18 +1,34 @@
-import {Link} from 'react-router-dom';
-import classes from '../styles/Videos.module.css'
-import Video from './Video';
-export default function Videos(){
-    return(
-        <div className={classes.videos}>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
-            <Link to="/quiz"><Video /> </Link>
+import { Link } from "react-router-dom";
+import useVideoList from "../hooks/useVideoList";
+import Video from "./Video";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState } from "react";
 
-        </div>
-    )
+export default function Videos() {
+  const [ page, setPage ] = useState(1);
+  const { loading, err, videos, hasMore } = useVideoList(page);
+  console.log("Page...: ", page);
+
+  return (
+    <div>
+      {/* videos.length > 0 && */}
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 8)}
+        >
+          {videos.map((video) => (
+            <Link to="/quiz" key={video.youtubeID}>
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            </Link>
+          ))}
+        </InfiniteScroll>
+      )}
+      {/* videos.length === 0 && */}
+      {!loading && <div> No Data Found </div>}
+      {err && <div> there is an error </div>}
+      {loading && <div> Content Loading.... </div>}
+    </div>
+  );
 }
